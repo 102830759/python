152dea69d44b58f8d24986b0c6f50c9fb9dc2ecb
huyue <102830759@qq.com>
2019-01-31 13:34:50 +0800

    Signed-off-by: huyue <102830759@qq.com> test

--- lianxi/Test011602.py
+++ lianxi/Test011602.py
@@ -1,4 +1,22 @@
+# Fibonacci series: 斐波纳契数列
+# 两个元素的总和确定了下一个数
 a, b = 0, 1
-while b < 10:
-    print(b)
-    a, b = b, a+b
+while b < 1000:
+    print(b, end=',')
+    a, b = b, a + b
+
+# 在 while … else 在条件语句为 false 时执行 else 的语句块：
+count = 0
+while count < 5:
+    print(count, " 小于 5")
+    count = count + 1
+else:
+    print(count, " 大于或等于 5")
+
+for letter in 'Runoob':
+    if letter == 'o':
+        pass
+        print('执行 pass 块')
+    print('当前字母 :', letter)
+
+print("Good bye!")
